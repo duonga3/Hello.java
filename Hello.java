d1a6b79bc22deb519f559a6a6dd2342784884db7
duonga3 <69981043+duonga3@users.noreply.github.com>
2020-08-20 12:27:27 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************************************
+*Hello.java
+********************************************************
+* Angel Duong
+* 8/20/20
+* 255 and 001
+********************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Angel Duong>");
+		System.out.println("<Hello.java>");
+		System.out.println("<First Lab>");
+		System.out.println("<8/20/20>");
+		System.out.println("<255 and 001>");
+		System.out.println();
+		}
+}
